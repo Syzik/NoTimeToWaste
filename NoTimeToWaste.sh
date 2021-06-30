4a443a01729d671ae00b288497c1d09927cc3e1c
syzik <syziksecu@protonmail.com>
2021-06-30 16:49:41 +0200

    space with pipe kali sucks

--- NoTimeToWaste.sh
+++ NoTimeToWaste.sh
@@ -36,6 +36,9 @@
 #  vnc
 #  x11 
 
+# vcenter 5480  
+# esxi 	902 903
+
 orange='\e[0;33m'
 neutre='\e[00m'
 rouge='\e[0;31m'
@@ -49,21 +52,21 @@ main()
 organiseClientSmb()
 {
 mkdir ./ALL/SMB/Client
-cat ./ALL/cmeSmbScan | grep "Windows XP \| Windows 7 \| Windows 10 \| Linux" --binary-files=text >> ./ALL/SMB/Client/ClientList
+cat ./ALL/cmeSmbScan |grep "Windows XP \| Windows 7 \| Windows 10 \| Linux" --binary-files=text >> ./ALL/SMB/Client/ClientList
 }
 
 organiseServerSmb()
 {
 mkdir ./ALL/SMB/Serv
-cat ./ALL/cmeSmbScan | grep 'Unix \| Windows Server \| Windows 5 \| Windows 6' --binary-files=text >> ./ALL/SMB/Serv/ServList
-cat ./ALL/SMB/Serv/ServList | grep "2003" --binary-files=text >> ./ALL/SMB/Serv/Serv2003
-cat ./ALL/SMB/Serv/Serv2003 | cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2003
-cat ./ALL/SMB/Serv/ServList | grep "2008" --binary-files=text >> ./ALL/SMB/Serv/Serv2008
-cat ./ALL/SMB/Serv/Serv2008 | cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2008
-cat ./ALL/SMB/Serv/ServList | grep "2012" --binary-files=text >> ./ALL/SMB/Serv/Serv2012
-cat ./ALL/SMB/Serv/Serv2012 | cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2012
-cat ./ALL/SMB/Serv/ServList | grep "2016" --binary-files=text >> ./ALL/SMB/Serv/Serv2016
-cat ./ALL/SMB/Serv/Serv2016 | cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2016
+cat ./ALL/cmeSmbScan |grep 'Unix \| Windows Server \| Windows 5 \| Windows 6' --binary-files=text >> ./ALL/SMB/Serv/ServList
+cat ./ALL/SMB/Serv/ServList |grep "2003" --binary-files=text >> ./ALL/SMB/Serv/Serv2003
+cat ./ALL/SMB/Serv/Serv2003 |cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2003
+cat ./ALL/SMB/Serv/ServList |grep "2008" --binary-files=text >> ./ALL/SMB/Serv/Serv2008
+cat ./ALL/SMB/Serv/Serv2008 |cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2008
+cat ./ALL/SMB/Serv/ServList |grep "2012" --binary-files=text >> ./ALL/SMB/Serv/Serv2012
+cat ./ALL/SMB/Serv/Serv2012 |cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2012
+cat ./ALL/SMB/Serv/ServList |grep "2016" --binary-files=text >> ./ALL/SMB/Serv/Serv2016
+cat ./ALL/SMB/Serv/Serv2016 |cut -d' ' -f10 >> ./ALL/SMB/Serv/ipServ2016
 }
 
 NmapServerSmb()
@@ -95,7 +98,7 @@ organizeAllFolder()
 {
 for proto in $protos
 do
-	cat ./*/$proto/ip$proto | sort -u > ./ALL/$proto/ip$proto
+	cat ./*/$proto/ip$proto |sort -u > ./ALL/$proto/ip$proto
 done
 }
 
@@ -104,29 +107,29 @@ organizeSubnetFolder()
 if [ "$input" ]; then 
 	for ip in $(cat $input)
 	do
-		cat $ip/masscanoutput | grep "21/tcp" | cut -f6 -d' ' > $ip/FTP/ipFTP
-		cat $ip/masscanoutput | grep "22/tcp" | cut -f6 -d' ' > $ip/SSH/ipSSH
-		cat $ip/masscanoutput | grep "23/tcp" | cut -f6 -d' ' > $ip/TELNET/ipTELNET
-		cat $ip/masscanoutput | grep "445/tcp" | cut -f6 -d' ' > $ip/SMB/ipSMB
-		cat $ip/masscanoutput | grep "88/tcp" | cut -f6 -d' ' > $ip/KERBEROS/ipKERBEROS
-		cat $ip/masscanoutput | grep " 389/tcp" | cut -f6 -d' ' > $ip/LDAP/ipLDAP
-		cat $ip/masscanoutput | grep "53/tcp" | cut -f6 -d' ' > $ip/DNS/ipDNS
-		cat $ip/masscanoutput | grep "143/tcp" | cut -f6 -d' ' >> $ip/IMAP/ipIMAP
-		cat $ip/masscanoutput | grep "993/tcp" | cut -f6 -d' ' >> $ip/IMAP/ipIMAP
-		cat $ip/masscanoutput | grep "25/tcp" | cut -f6 -d' ' >> $ip/IMAP/ipSMTP
-		cat $ip/masscanoutput | grep "465/tcp" | cut -f6 -d' ' >> $ip/IMAP/ipSMTP
-		cat $ip/masscanoutput | grep "110/tcp" | cut -f6 -d' ' >> $ip/IMAP/ipPOP3
-		cat $ip/masscanoutput | grep "995/tcp" | cut -f6 -d' ' >> $ip/IMAP/ipPOP3
-		cat $ip/masscanoutput | grep "17990/tcp" | cut -f6 -d' ' > $ip/ILO/ipILO
-		cat $ip/masscanoutput | grep "5900/tcp" | cut -f6 -d' ' > $ip/IDRAC/ipIDRAC
-		cat $ip/masscanoutput | grep "1433/tcp" | cut -f6 -d' ' > $ip/SQL/ipSQL
-		cat $ip/masscanoutput | grep "3306/tcp" | cut -f6 -d' ' > $ip/SQL/ipMYSQL
-		cat $ip/masscanoutput | grep "1521/tcp" | cut -f6 -d' ' > $ip/SQL/ipORACLE
-		cat $ip/masscanoutput | grep "139/tcp" | cut -f6 -d' ' > $ip/NETBIOS/ipNETBIOS
-		cat $ip/masscanoutput | grep "443/tcp" | cut -f6 -d' ' > $ip/HTTPS/ipHTTPS
-		cat $ip/masscanoutput | grep "80/tcp" | cut -f6 -d' ' > $ip/HTTP/ipHTTP
-		cat $ip/masscanoutput | grep "3389/tcp" | cut -f6 -d' ' > $ip/RDP/ipRDP
-		cat $ip/masscanoutput | grep "5001/tcp" | cut -f6 -d' ' > $ip/JAVACONSOLE/ipJAVACONSOLE
+		cat $ip/masscanoutput |grep "21/tcp" |cut -f6 -d' ' > $ip/FTP/ipFTP
+		cat $ip/masscanoutput |grep "22/tcp" |cut -f6 -d' ' > $ip/SSH/ipSSH
+		cat $ip/masscanoutput |grep "23/tcp" |cut -f6 -d' ' > $ip/TELNET/ipTELNET
+		cat $ip/masscanoutput |grep "445/tcp" |cut -f6 -d' ' > $ip/SMB/ipSMB
+		cat $ip/masscanoutput |grep "88/tcp" |cut -f6 -d' ' > $ip/KERBEROS/ipKERBEROS
+		cat $ip/masscanoutput |grep " 389/tcp" |cut -f6 -d' ' > $ip/LDAP/ipLDAP
+		cat $ip/masscanoutput |grep "53/tcp" |cut -f6 -d' ' > $ip/DNS/ipDNS
+		cat $ip/masscanoutput |grep "143/tcp" |cut -f6 -d' ' >> $ip/IMAP/ipIMAP
+		cat $ip/masscanoutput |grep "993/tcp" |cut -f6 -d' ' >> $ip/IMAP/ipIMAP
+		cat $ip/masscanoutput |grep "25/tcp" |cut -f6 -d' ' >> $ip/IMAP/ipSMTP
+		cat $ip/masscanoutput |grep "465/tcp" |cut -f6 -d' ' >> $ip/IMAP/ipSMTP
+		cat $ip/masscanoutput |grep "110/tcp" |cut -f6 -d' ' >> $ip/IMAP/ipPOP3
+		cat $ip/masscanoutput |grep "995/tcp" |cut -f6 -d' ' >> $ip/IMAP/ipPOP3
+		cat $ip/masscanoutput |grep "17990/tcp" |cut -f6 -d' ' > $ip/ILO/ipILO
+		cat $ip/masscanoutput |grep "5900/tcp" |cut -f6 -d' ' > $ip/IDRAC/ipIDRAC
+		cat $ip/masscanoutput |grep "1433/tcp" |cut -f6 -d' ' > $ip/SQL/ipSQL
+		cat $ip/masscanoutput |grep "3306/tcp" |cut -f6 -d' ' > $ip/SQL/ipMYSQL
+		cat $ip/masscanoutput |grep "1521/tcp" |cut -f6 -d' ' > $ip/SQL/ipORACLE
+		cat $ip/masscanoutput |grep "139/tcp" |cut -f6 -d' ' > $ip/NETBIOS/ipNETBIOS
+		cat $ip/masscanoutput |grep "443/tcp" |cut -f6 -d' ' > $ip/HTTPS/ipHTTPS
+		cat $ip/masscanoutput |grep "80/tcp" |cut -f6 -d' ' > $ip/HTTP/ipHTTP
+		cat $ip/masscanoutput |grep "3389/tcp" |cut -f6 -d' ' > $ip/RDP/ipRDP
+		cat $ip/masscanoutput |grep "5001/tcp" |cut -f6 -d' ' > $ip/JAVACONSOLE/ipJAVACONSOLE
 	done
 fi
 organizeAllFolder
@@ -147,10 +150,10 @@ if [ "$input" ]; then
 	       echo -e "Scanning $ip /16 in progress"
 	       echo -e "${orange}####################################${neutre}"
 	       if [ "$rate" ]; then
-		      masscan -p 5001,445,80,88,389,53,143,993,110,995,25,465,17990,5900,1433,3306,1521,139,443,21,22,23,3389 $ip/16 --rate=$rate | tee $ip/masscanoutput
+		      masscan -p 5001,445,80,88,389,53,143,993,110,995,25,465,17990,5900,1433,3306,1521,139,443,21,22,23,3389 $ip/16 --rate=$rate |tee $ip/masscanoutput
 	      else 
 		      #masscan -p 5001,445,80,88,389,53,143,993,110,995,25,465,17990,5900,1433,3306,1521,139,443,21,22,23,3389 172.19.91.1-31 --rate=50000 | tee $ip/masscanoutput
-		      masscan -p 5001,445,80,88,389,53,143,993,110,995,25,465,17990,5900,1433,3306,1521,139,443,21,22,23,3389 $ip/16 | tee $ip/masscanoutput
+		      masscan -p 5001,445,80,88,389,53,143,993,110,995,25,465,17990,5900,1433,3306,1521,139,443,21,22,23,3389 $ip/16 |tee $ip/masscanoutput
 	       fi
        done
 fi
